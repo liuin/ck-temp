dd7abf3882ba84d64bfb5fd33fdda42aaba21e40
liuin <20893791@qq.com>
2017-02-24 18:10:20 +0800

    0224

--- framework/suanfa/js.js
+++ framework/suanfa/js.js
@@ -4,9 +4,9 @@
       for (var i = 0;  i<str.length ; i++) {
         if ((str.charCodeAt(i) >= "A".charCodeAt()) && (str.charCodeAt(i) <= "z".charCodeAt())) {
           if ((str.charCodeAt(i) >= "A".charCodeAt()) && (str.charCodeAt(i) <= "Z".charCodeAt())) {
-            strCase += String.fromCharCode(str.charCodeAt(i) + 32);            
+            strCase += String.fromCharCode(str.charCodeAt(i) + 32);
           }else {
-            strCase += str.charAt(i);            
+            strCase += str.charAt(i);
           }
         }else {
           strCase += str.charAt(i);
